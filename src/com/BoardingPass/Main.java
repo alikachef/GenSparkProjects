bb6e292974e983b63450ae55dad1adf450a84eae
robertcwarren1@gmail.com <bobbywarren14@gmail.com>
2022-06-15 12:47:17 -0700

    -Added GUI.
    -GUI input is stored in boarding pass variables.
    
    -Enums are not used because they could not be converted to string so I replaced them with DefaultComboBoxes.
    
    -DefaultComboBoxes populate the seat combo box dependent on whether Business or Economy class is chosen.
    
    -Book button executes everything that was in Main via ActionListener and also performs System.exit when clicked.
    
    -Couldnt figure out how to implement the randomized seats to populate the seat combobox.

--- src/com/BoardingPass/Main.java
+++ src/com/BoardingPass/Main.java
@@ -1,40 +1,11 @@
 package BoardingPass;
 
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner userInput = new Scanner(System.in);
-
-        System.out.println("Enter Name");
-        String name = userInput.nextLine();
-
-        System.out.println("Enter Gender");
-        String gender = userInput.nextLine();
-
-        System.out.println("Enter age");
-        String age = userInput.nextLine();
-
-        System.out.println("Enter Email");
-        String email = userInput.nextLine();
-
-        System.out.println("Enter Phone Number");
-        String phone = userInput.nextLine();
-
-        System.out.println("Choose Flight Class (B for Business or E for Economy)");
-        String type = userInput.nextLine();
-
-        System.out.println("Enter From Location");
-        String from = userInput.nextLine();
-
-        System.out.println("Enter Destination");
-        String destination = userInput.nextLine();
-
-
-       BoardingPass pass1 = new BoardingPass(type, name,  from, destination,age,email,gender, phone);
-       pass1.generatePass();
-         pass1.calculatePrice();
-       pass1.writeImage();
+        new BoardingFrame();
     }
 }
